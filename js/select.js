9b0ec1ca3e7ad1efbe931878a4381e71c6c34921
Jahidul002 <emon.me44@gmail.com>
2022-08-22 13:06:02 +0600

    select js add

--- js/select.js
+++ js/select.js
@@ -1,4 +1,28 @@
+const playerLength = [];
+
+function displayName(allPlayer) {
+    console.log(allPlayer);
+    const playerField = document.getElementById('select-field')
+    playerField.innerHTML = "";
+    for (let i = 0; i < playerLength.length; i++) {
+        if (playerLength.length >= 6) {
+            break
+        }
+        let serial = i + 1;
+        let index = playerLength[i];
+        // console.log();
+        const p = document.createElement('p')
+        p.innerText = `
+        ${serial}   ${index}
+        
+        `;
+        playerField.appendChild(p)
+    }
+}
+
 function addToSelect(element) {
     const playerName = element.parentNode.children[0].innerText;
-    console.log(playerName);
+    playerLength.push(playerName)
+
+    displayName(playerLength)
 }
