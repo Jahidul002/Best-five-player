e0552720c039f5d9a5e70b4c069d7a2beb1b6bda
Jahidul002 <emon.me44@gmail.com>
2022-08-23 23:55:43 +0600

    last update 11:55pm

--- js/calculate.js
+++ js/calculate.js
@@ -9,17 +9,34 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     const playerCost = playerAmmount * perPlayerField
     playerExpenses.innerText = playerCost;
-    cost.push(playerCost)
+    // cost.push(playerCost)
 })
 document.getElementById('total-btn').addEventListener('click', function () {
+
+    const perPlayerField = getFieldValueById('per-PlayerField');
+    const playerAmmount = playerArray.length
+
+    const playerExpenses = document.getElementById('player-expenses')
+    const playerExpensesString = playerExpenses.innerText;
+
+    const playerCost = playerAmmount * perPlayerField
+    playerExpenses.innerText = playerCost;
+
+
+
+
+
+
+
+
     const managerField = getFieldValueById('manager-field');
     const coachField = getFieldValueById('coach-field');
 
     const totalBalance = document.getElementById('total-balance');
     const blnString = totalBalance.innerText;
 
-    const prevEx = cost[0];
-    const totalCost = prevEx + managerField + coachField
+
+    const totalCost = playerCost + managerField + coachField
 
     totalBalance.innerText = totalCost;
 
